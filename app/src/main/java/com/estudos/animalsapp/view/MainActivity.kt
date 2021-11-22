7ef80ffe3d7c9e3b86d5d154661747a64a169249
Mara Rosa <edimara.rosa@outlook.com>
2021-11-22 19:02:16 -0300

    navigation configuration

--- app/src/main/java/com/estudos/animalsapp/view/MainActivity.kt
+++ app/src/main/java/com/estudos/animalsapp/view/MainActivity.kt
@@ -1,9 +1,10 @@
- package com.estudos.animalsapp
+ package com.estudos.animalsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.estudos.animalsapp.R
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
