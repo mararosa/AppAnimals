a1730755c48acfdae782498609d17687380a72fa
Mara Rosa <edimara.rosa@outlook.com>
2021-12-22 16:53:04 -0300

    navigation configuration

--- app/src/main/java/com/estudos/animalsapp/view/ListFragment.kt
+++ app/src/main/java/com/estudos/animalsapp/view/ListFragment.kt
@@ -38,6 +38,10 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
