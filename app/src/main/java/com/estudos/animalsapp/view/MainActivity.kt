ce091902fbd9edd505e345f7f61f807bc789a09c
Mara Rosa <edimara.rosa@outlook.com>
2021-12-27 17:40:11 -0300

    navigation actions

--- app/src/main/java/com/estudos/animalsapp/view/MainActivity.kt
+++ app/src/main/java/com/estudos/animalsapp/view/MainActivity.kt
@@ -1,12 +1,25 @@
- package com.estudos.animalsapp.view
+package com.estudos.animalsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.estudos.animalsapp.R
 
- class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragmentContainerView)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
