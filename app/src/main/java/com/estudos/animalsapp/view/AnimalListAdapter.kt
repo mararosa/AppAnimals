dfb5e977d7784b1bfd94f2d5dc982cb768e1d2e4
Mara Rosa <edimara.rosa@outlook.com>
2021-12-29 20:03:56 -0300

    add observers, putting it all together.

--- app/src/main/java/com/estudos/animalsapp/view/AnimalListAdapter.kt
+++ app/src/main/java/com/estudos/animalsapp/view/AnimalListAdapter.kt
@@ -5,9 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.estudos.animalsapp.R
+import com.estudos.animalsapp.databinding.ItemAnimalBinding
 import com.estudos.animalsapp.model.Animal
 
-class AnimalListAdapter(private val animalList: ArrayList<Animal>):
+class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
     RecyclerView.Adapter<AnimalListAdapter.AnimalViewHolder>() {
 
     fun updateAnimalList(newAnimalList: List<Animal>) {
@@ -17,16 +18,21 @@ class AnimalListAdapter(private val animalList: ArrayList<Animal>):
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_animal, parent, false)
-        return AnimalViewHolder(view)
+        val binding = ItemAnimalBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return AnimalViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
-//        holder.view.animalName.text = animalList[position].name
+        with(holder) {
+            with(animalList[position]) {
+                binding.animalName.text = name
+            }
+        }
     }
 
     override fun getItemCount() = animalList.size
 
-    class AnimalViewHolder(var view: View): RecyclerView.ViewHolder(view)
+    inner class AnimalViewHolder(var binding: ItemAnimalBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
--- app/src/main/java/com/estudos/animalsapp/view/ListFragment.kt
+++ app/src/main/java/com/estudos/animalsapp/view/ListFragment.kt
@@ -5,14 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
 import com.estudos.animalsapp.R
 import com.estudos.animalsapp.databinding.FragmentListBinding
+import com.estudos.animalsapp.model.Animal
+import com.estudos.animalsapp.viewmodel.ListViewModel
 
 class ListFragment : Fragment() {
 
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: ListViewModel
+    private val listAdapter = AnimalListAdapter(arrayListOf())
+
+    private val animalListDataObserver = Observer<List<Animal>> { list ->
+        list?.let {
+            binding.animalList.visibility = View.VISIBLE
+            listAdapter.updateAnimalList(it)
+        }
+    }
+    private val loadingLiveDataObserver = Observer<Boolean> { isLoading ->
+        binding.loadingView.visibility = if (isLoading) View.VISIBLE else View.GONE
+        if (isLoading) {
+            binding.listError.visibility = View.GONE
+            binding.animalList.visibility = View.GONE
+        }
+    }
+    private val errorLiveDataObserver = Observer<Boolean> { isError ->
+        binding.listError.visibility = if (isError) View.VISIBLE else View.GONE
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +47,21 @@ class ListFragment : Fragment() {
         _binding = FragmentListBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.animals.observe(this, animalListDataObserver)
+        viewModel.loading.observe(this, loadingLiveDataObserver)
+        viewModel.loadError.observe(this, errorLiveDataObserver)
+        viewModel.refresh()
+
+        binding.animalList.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = listAdapter
+        }
     }
 
     override fun onDestroyView() {
