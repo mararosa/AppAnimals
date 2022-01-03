8024106ab64f96f02d6a60744b53a0c0857a4d81
Mara Rosa <edimara.rosa@outlook.com>
2022-01-02 21:36:36 -0300

    add glide

--- app/src/main/java/com/estudos/animalsapp/view/AnimalListAdapter.kt
+++ app/src/main/java/com/estudos/animalsapp/view/AnimalListAdapter.kt
@@ -1,10 +1,15 @@
 package com.estudos.animalsapp.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.estudos.animalsapp.R
 import com.estudos.animalsapp.databinding.ItemAnimalBinding
 import com.estudos.animalsapp.model.Animal
+import com.estudos.animalsapp.util.getProgressDrawable
+import com.estudos.animalsapp.util.loadImage
+import kotlinx.android.synthetic.main.item_animal.view.*
 
 class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
     RecyclerView.Adapter<AnimalListAdapter.AnimalViewHolder>() {
@@ -16,21 +21,33 @@ class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalViewHolder {
-        val binding = ItemAnimalBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-        return AnimalViewHolder(binding)
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_animal, parent, false)
+        return AnimalViewHolder(view)
+//        val binding = ItemAnimalBinding
+//            .inflate(LayoutInflater.from(parent.context), parent, false)
+//        return AnimalViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
         with(holder) {
             with(animalList[position]) {
-                binding.animalName.text = name
+                view.animalName.text = name
+                view.animalImage.loadImage(imageUrl, getProgressDrawable(holder.view.context))
+
+                //TODO como pegar context usando o binding, nao consegui e ai voltei pro synthetic
+//                binding.animalName.text = name
+//                binding.animalImage.loadImage(imageUrl, getProgressDrawable())
             }
         }
     }
 
     override fun getItemCount() = animalList.size
 
-    inner class AnimalViewHolder(var binding: ItemAnimalBinding) :
-        RecyclerView.ViewHolder(binding.root)
+
+    inner class AnimalViewHolder(var view: View) :
+        RecyclerView.ViewHolder(view)
+
+//    inner class AnimalViewHolder(var binding: ItemAnimalBinding) :
+//        RecyclerView.ViewHolder(binding.root)
 }
