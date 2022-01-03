f0c956f613c95f16fa8a881f2cf7522867a37a37
Mara Rosa <edimara.rosa@outlook.com>
2022-01-03 02:55:32 -0300

    add palette library

--- app/src/main/java/com/estudos/animalsapp/view/DetailFragment.kt
+++ app/src/main/java/com/estudos/animalsapp/view/DetailFragment.kt
@@ -1,11 +1,17 @@
 package com.estudos.animalsapp.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.estudos.animalsapp.R
 import com.estudos.animalsapp.databinding.FragmentDetailBinding
 import com.estudos.animalsapp.model.Animal
@@ -38,6 +44,25 @@ class DetailFragment : Fragment() {
         }
 
         setupView()
+        animal?.imageUrl?.let {
+            setBackgroundColor(it)
+        }
+    }
+
+    private fun setBackgroundColor(imageUrl: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(imageUrl)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate() { palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+                            binding.animalDetailLayout.setBackgroundColor(intColor)
+                        }
+                }
+                override fun onLoadCleared(placeholder: Drawable?) {}
+            })
     }
 
     private fun setupView() {
