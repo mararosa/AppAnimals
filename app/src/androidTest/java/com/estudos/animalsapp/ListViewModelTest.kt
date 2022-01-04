ad35ac24be9a81c6ec53499eb7efa0997af2e54d
Mara Rosa <edimara.rosa@outlook.com>
2022-01-03 21:55:50 -0300

    create the test rules

---
+++ app/src/androidTest/java/com/estudos/animalsapp/ListViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.estudos.animalsapp
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Before
+import org.junit.Rule
+import java.util.concurrent.Executor
+
+class ListViewModelTest {
+
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupRxSchedulers() {
+        val immediate = object : Scheduler() {
+            override fun createWorker(): Worker {
+                return ExecutorScheduler.ExecutorWorker(Executor { it.run() }, true)
+            }
+        }
+
+        //Create Threads return immediate when call is being made
+        RxJavaPlugins.setInitIoSchedulerHandler { scheduler -> immediate }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> immediate }
+
+    }
+}
